80248acefdbec5c7d0af2da9b615c6b6c1642b66
Hannah Maschke <hannah.maschke@dcmail.ca>
2025-01-27 15:47:23 -0500

    fixed the error message issue
    and got the repository working again

---
+++ AutismBehaviourTracker/aboutAutismForm.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace AutismBehaviourTracker
+{
+    partial class aboutAutismForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(aboutAutismForm));
+            this.cancelButton = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.aboutAutismLabel = new System.Windows.Forms.Label();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.SuspendLayout();
+            // 
+            // cancelButton
+            // 
+            this.cancelButton.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.cancelButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.cancelButton.Location = new System.Drawing.Point(12, 646);
+            this.cancelButton.Name = "cancelButton";
+            this.cancelButton.Size = new System.Drawing.Size(165, 35);
+            this.cancelButton.TabIndex = 0;
+            this.cancelButton.Text = "Back to main page";
+            this.cancelButton.UseVisualStyleBackColor = false;
+            this.cancelButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(25, 150);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(814, 150);
+            this.textBox1.TabIndex = 1;
+            // 
+            // aboutAutismLabel
+            // 
+            this.aboutAutismLabel.AutoSize = true;
+            this.aboutAutismLabel.BackColor = System.Drawing.Color.Transparent;
+            this.aboutAutismLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.aboutAutismLabel.Location = new System.Drawing.Point(319, 55);
+            this.aboutAutismLabel.Name = "aboutAutismLabel";
+            this.aboutAutismLabel.Size = new System.Drawing.Size(210, 37);
+            this.aboutAutismLabel.TabIndex = 2;
+            this.aboutAutismLabel.Text = "About Autism";
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(239, 329);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(470, 273);
+            this.webBrowser1.TabIndex = 3;
+            this.webBrowser1.Url = new System.Uri("", System.UriKind.Relative);
+            this.webBrowser1.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.webBrowser1_DocumentCompleted);
+            // 
+            // aboutAutismForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.CancelButton = this.cancelButton;
+            this.ClientSize = new System.Drawing.Size(872, 693);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.aboutAutismLabel);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.cancelButton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "aboutAutismForm";
+            this.Text = "About Autism";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button cancelButton;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label aboutAutismLabel;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+    }
+}
---
+++ AutismBehaviourTracker/journalEntry.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AutismBehaviourTracker
+{
+    public partial class journalEntry : Form
+    {
+        public journalEntry()
+        {
+            InitializeComponent();
+        }
+
+        private void submitButton_Click(object sender, EventArgs e)
+        {
+            // Get the current date and time
+            string currentDate = DateTime.Now.ToString("MMMM dd, yyyy HH:mm:ss");
+
+            // Add the date to the top of each journal post
+            string entryWithDate = $"Date: {currentDate}\n{newEntryTextBox.Text}\n\n" + "\n\n ---------------------------------------------------------------------------------------";
+
+            // when the user hits submit, the entry is saved to the previous entries page
+            previousEntryTextBox.Text += entryWithDate;
+
+            // clear the journal entry text box for the next entry
+            newEntryTextBox.Clear();
+        }
+
+        private void cancelButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
